592dcfa867828fa8dc0dcb082bc0e904d973e8e3
hjd94 <112848877+hjd94@users.noreply.github.com>
2022-09-08 19:05:51 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,33 @@
-from nltk.tokenize import sent_tokenize
+fimport nltk
 import os 
 import PyPDF2
 import pandas
 
-# UNcomment to download the punkt dataset for the nltk package.
 # ___________________________________
 # import nltk
 # nltk.download('punkt')
 # ___________________________________
 
 
-folder = ""
-Key_words = []
-save_file = ""
+folder_path = r""
+Key_words = ["", ""]
+output_name = ""
 
 sentences_list = []
 empty_pages = set()
-
-files = os.listdir(folder)
-files = [file for file in files if file.lower().endswith(".pdf")] #check if valid path
-for file in files:
-    pdf_submission = PyPDF2.PdfReader(f"{folder}/{file}") #use os.path.join
+file_names = os.listdir(folder_path)
+file_names = [file_name for file_name in file_names if file_name.lower().endswith(".pdf")]
+for file_name in file_names:
+    pdf_submission = PyPDF2.PdfReader(os.join(folder_path, file_name))
     for page in pdf_submission.pages:
         page_text = page.extractText()
-        tokenize_sentence = sent_tokenize(page_text)
+        tokenize_sentence = nltk.tokenize.sent_tokenize(page_text)
         if len(tokenize_sentence) == 0:
-            empty_pages = empty_pages.union({file})
-        sentences = [[file, sentence] for sentence in tokenize_sentence if any(word in sentence for word in Key_words)]
-        sentences_list = sentences_list + sentences
+            empty_pages = empty_pages.union({file_name})
+        sentences = [[file_name, sentence] for sentence in tokenize_sentence if any(word in sentence for word in Key_words)]
+        sentences_list.extend(sentences)
 
-print(f"Number of file with at least one bank page: {len(empty_pages)}")
+print(f"Number of files with at least one bank page: {len(empty_pages)}")
 print(empty_pages)
-df = pandas.DataFrame (sentences_list, columns = ['file', 'sentence'])
-df.to_csv(save_file,  encoding= "utf-8", index= False)
+df = pandas.DataFrame (sentences_list, columns=['file', 'sentence'])
+df.to_csv(output_name,  encoding="utf-8", index=False)
